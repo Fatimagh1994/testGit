8d353011677764c095382bf8ee008283ba89365a
Fatima Ghaddar <fatima.g94@hotmail.com>
2021-06-30 14:03:35 -0400

    Deployer projet sur GitHub

---
+++ src/main/java/com/mycompany/maven_project_jenkins/CalculerImp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.maven_project_jenkins;
+
+/**
+ *
+ * @author fatim
+ */
+public class CalculerImp {
+    
+    public int  calculer(){
+        int a=5;
+        int b=10;
+        int result = a +b ;
+         return result;
+    }
+    
+}
---
+++ src/test/java/com/mycompany/maven_project_jenkins/CalculerImpIT.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.maven_project_jenkins;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author fatim
+ */
+public class CalculerImpIT {
+    
+    public CalculerImpIT() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
